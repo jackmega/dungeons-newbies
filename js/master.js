2a5a632ff7d8e05466d63f03f8bbfc194b3d990a
jackmega <jackmega@live.com>
2018-10-02 18:31:49 -0600

    Updated the entire website

--- js/master.js
+++ js/master.js
@@ -1,12 +1,8 @@
-function showNav() {
-	document.getElementById('navigation').style.display = 'none';
-	document.getElementById('show-btn').style.display = 'none';
-	document.getElementById('hide-btn').style.display = 'block';
-	document.getElementById('mobile-navigation').style.display = 'block';
-}
-function hideNav() {
-	document.getElementById('navigation').style.display = 'block';
-	document.getElementById('show-btn').style.display = 'block';
-	document.getElementById('hide-btn').style.display = 'none';
-	document.getElementById('mobile-navigation').style.display = 'none';
+function myFunction() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
 }
